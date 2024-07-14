9c1e8cc774932d31828d65f4ea2b63fb1403edf0
Karl <57076645+ayz1e@users.noreply.github.com>
2024-07-14 00:56:57 -0400

    Update loadNavbar.js

--- assets/js/loadNavbar.js
+++ assets/js/loadNavbar.js
@@ -1,14 +1,22 @@
 // loadNavbar.js
 async function loadNavbar() {
-    const response = await fetch('navbar.html');
-    const navbarHTML = await response.text();
-    document.getElementById('navbar-container').innerHTML = navbarHTML;
+    console.log('Loading navbar...');
+    try {
+        const response = await fetch('navbar.html');
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        const navbarHTML = await response.text();
+        document.getElementById('navbar-container').innerHTML = navbarHTML;
 
-    // Set the username in the navbar and sidebar
-    const user = JSON.parse(localStorage.getItem('user'));
-    if (user && user.Username) {
-        document.getElementById('welcome-user').textContent = user.Username;
-        document.getElementById('welcome-user-sidebar').textContent = user.Username;
+        // Set the username in the navbar and sidebar
+        const user = JSON.parse(localStorage.getItem('user'));
+        if (user && user.Username) {
+            document.getElementById('welcome-user').textContent = user.Username;
+            document.getElementById('welcome-user-sidebar').textContent = user.Username;
+        }
+    } catch (error) {
+        console.error('Error loading navbar:', error);
     }
 }
 
